31d9f6afce2effd65367a21ae4792dc29d05416b
Judah Ben <judahben149@gmail.com>
2024-09-01 01:03:42 +0100

    App default files and configs too

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Thirdparty dependencies of TBD projects not in Maven Central
+        maven("https://blockxyz.jfrog.io/artifactory/tbd-oss-thirdparty-maven2/")
+    }
+}
+
+rootProject.name = "FourthWall"
+include(":app")
+ 
