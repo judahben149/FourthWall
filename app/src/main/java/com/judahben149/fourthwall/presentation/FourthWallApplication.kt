93bae9f8c5a32d3b6a2da04e27389e091c844d1e
Judah Ben <judahben149@gmail.com>
2024-09-01 19:01:58 +0100

    Setup Hilt and basic navigation

---
+++ app/src/main/java/com/judahben149/fourthwall/presentation/FourthWallApplication.kt
@@ -0,0 +1,9 @@
+package com.judahben149.fourthwall.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class FourthWallApplication: Application() {
+}
--- app/src/main/java/com/judahben149/fourthwall/presentation/MainActivity.kt
+++ app/src/main/java/com/judahben149/fourthwall/presentation/MainActivity.kt
@@ -6,16 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.judahben149.fourthwall.R
+import com.judahben149.fourthwall.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = getResources().getColor(R.color.black)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
