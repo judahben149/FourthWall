fd8dcd5f831f61f89ffc523f455e40e3b475716e
Judah Ben <judahben149@gmail.com>
2024-09-01 01:04:10 +0100

    Added home and onboarding fragments and resources

---
+++ app/src/main/java/com/judahben149/fourthwall/presentation/onboarding/Onboarding2Fragment.kt
@@ -0,0 +1,22 @@
+package com.judahben149.fourthwall.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.judahben149.fourthwall.R
+
+
+class Onboarding2Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding2, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/judahben149/fourthwall/presentation/onboarding/Onboarding3Fragment.kt
@@ -0,0 +1,21 @@
+package com.judahben149.fourthwall.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.judahben149.fourthwall.R
+
+
+class Onboarding3Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding3, container, false)
+    }
+
+}
