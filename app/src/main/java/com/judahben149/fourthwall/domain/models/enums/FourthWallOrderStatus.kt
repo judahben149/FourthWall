fd5a61df4fb4535f53ec34bdef6632559d2056af
Judah Ben <judahben149@gmail.com>
2024-09-02 11:26:59 +0100

    Defined enums for certain "states"

---
+++ app/src/main/java/com/judahben149/fourthwall/domain/models/enums/FourthWallOrderStatus.kt
@@ -0,0 +1,7 @@
+package com.judahben149.fourthwall.domain.models.enums
+
+enum class FourthWallOrderStatus {
+    PENDING,
+    CLOSED,
+    COMPLETED
+}
---
+++ app/src/main/java/com/judahben149/fourthwall/domain/models/enums/PayOutMethods.kt
@@ -0,0 +1,7 @@
+package com.judahben149.fourthwall.domain.models.enums
+
+enum class PayOutMethods {
+    CARD,
+    BANK,
+    STORED_BALANCE
+}
