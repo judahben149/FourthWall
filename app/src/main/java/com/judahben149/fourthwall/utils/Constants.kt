51882b2d39ca84d6e58da47d7f6723654097d496
Judah Ben <judahben149@gmail.com>
2024-09-02 23:28:03 +0100

    Updated constants

--- app/src/main/java/com/judahben149/fourthwall/utils/Constants.kt
+++ app/src/main/java/com/judahben149/fourthwall/utils/Constants.kt
@@ -1,7 +1,12 @@
 package com.judahben149.fourthwall.utils
 
 object Constants {
+    // DI Module provider names
+    const val FOURTH_WALL_SHARED_PREF = "FOURTH_WALL_SHARED_PREF"
 
-
+    // Database
     const val DATABASE_NAME = "FOURTH_WALL_ROOM_DATABASE"
+
+    //Pref Keys
+    const val HAS_COMPLETED_ONBOARDING = "HAS_COMPLETED_ONBOARDING"
 }
