2de377f362808ba2b47f2a7d43d54ff6027ce442
Judah Ben <judahben149@gmail.com>
2024-09-02 23:32:03 +0100

    Converted Onboarding fragment to activity for better feature segmentation

---
+++ app/src/main/java/com/judahben149/fourthwall/presentation/onboarding/OnboardingActivity.kt
@@ -0,0 +1,93 @@
+package com.judahben149.fourthwall.presentation.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.judahben149.fourthwall.R
+import com.judahben149.fourthwall.databinding.ActivityOnboardingBinding
+import com.judahben149.fourthwall.domain.SessionManager
+import com.judahben149.fourthwall.presentation.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class OnboardingActivity : AppCompatActivity() {
+
+
+    private var _binding: ActivityOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var sessionManager: SessionManager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        _binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+
+        val viewPager: ViewPager2 = binding.viewPager
+        val adapter = OnboardingPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        setupPageChangeCallback()
+
+        binding.run {
+            btnNext.setOnClickListener {
+                if (viewPager.currentItem < 2) {
+                    viewPager.currentItem += 1
+                } else {
+                    navigateToHomeScreen()
+                }
+            }
+        }
+    }
+
+    private fun setupPageChangeCallback() {
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                updateButtonText(position)
+            }
+        })
+    }
+
+    private fun updateButtonText(position: Int) {
+        when (position) {
+            2 -> binding.btnNext.text = getString(R.string.get_started)
+            else -> binding.btnNext.text = getString(R.string.next)
+        }
+    }
+
+    private fun navigateToHomeScreen() {
+        sessionManager.updateHasCompletedOnboarding(true)
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
