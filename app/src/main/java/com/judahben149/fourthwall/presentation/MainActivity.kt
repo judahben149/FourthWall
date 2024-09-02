c31ca0bea16f6d9e3d2aa192a64f9cd0d8451994
Judah Ben <judahben149@gmail.com>
2024-09-02 23:27:25 +0100

    Setup bottom navigation fragments

--- app/src/main/java/com/judahben149/fourthwall/presentation/MainActivity.kt
+++ app/src/main/java/com/judahben149/fourthwall/presentation/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import com.judahben149.fourthwall.R
 import com.judahben149.fourthwall.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +16,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
-    val binding get() = _binding!!
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+    private var isCurrentlyNavigatingFromBottomNav = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +28,53 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        window.statusBarColor = getResources().getColor(R.color.black)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setOnItemSelectedListener { menuItem ->
+            if (!isCurrentlyNavigatingFromBottomNav) {
+                isCurrentlyNavigatingFromBottomNav = true
+                when(menuItem.itemId) {
+                    R.id.navigation_home -> navigateToTopLevelDestination(R.id.homeFragment)
+                    R.id.navigation_orders -> navigateToTopLevelDestination(R.id.ordersFragment)
+                    R.id.navigation_dashboard -> navigateToTopLevelDestination(R.id.dashboardFragment)
+                }
+                isCurrentlyNavigatingFromBottomNav = false
+                true
+            } else {
+                false
+            }
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (!isCurrentlyNavigatingFromBottomNav) {
+                when (destination.id) {
+                    R.id.homeFragment -> binding.bottomNav.selectedItemId = R.id.navigation_home
+                    R.id.ordersFragment -> binding.bottomNav.selectedItemId = R.id.navigation_orders
+                    R.id.dashboardFragment -> binding.bottomNav.selectedItemId = R.id.navigation_dashboard
+                }
+            }
+        }
+    }
+
+    private fun navigateToTopLevelDestination(destinationId: Int) {
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(navController.graph.startDestinationId, inclusive = false, saveState = true)
+            .setLaunchSingleTop(true)
+            .setRestoreState(true)
+            .build()
+
+        navController.navigate(destinationId, null, navOptions)
     }
 
     override fun onDestroy() {
