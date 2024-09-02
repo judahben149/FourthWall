4d3a511a39f4a097fe89ce45540c91352754f762
Judah Ben <judahben149@gmail.com>
2024-09-02 11:23:06 +0100

    Setup room database module for dependency injection

---
+++ app/src/main/java/com/judahben149/fourthwall/di/module/DatabaseModule.kt
@@ -0,0 +1,43 @@
+package com.judahben149.fourthwall.di.module
+
+import android.content.Context
+import androidx.room.Room
+import com.judahben149.fourthwall.data.local.FourthWallDatabase
+import com.judahben149.fourthwall.data.local.OrderDao
+import com.judahben149.fourthwall.data.local.UserAccountDao
+import com.judahben149.fourthwall.utils.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providesFourthWallDatabase(@ApplicationContext context: Context): FourthWallDatabase {
+
+        return Room.databaseBuilder(
+            context,
+            FourthWallDatabase::class.java,
+            DATABASE_NAME
+        )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesOrderDao(fourthWallDatabase: FourthWallDatabase): OrderDao {
+        return fourthWallDatabase.orderDao()
+    }
+
+    @Provides
+    @Singleton
+    fun providesUserAccountDao(fourthWallDatabase: FourthWallDatabase): UserAccountDao {
+        return fourthWallDatabase.userAccountDao()
+    }
+}
