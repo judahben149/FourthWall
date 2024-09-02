88bf415fa3e5a25a0711487c3c469e8e7e5309a8
Judah Ben <judahben149@gmail.com>
2024-09-02 11:25:56 +0100

    Created use-cases to make plug & play easy

---
+++ app/src/main/java/com/judahben149/fourthwall/domain/usecase/GetAllOrdersUseCase.kt
@@ -0,0 +1,14 @@
+package com.judahben149.fourthwall.domain.usecase
+
+import com.judahben149.fourthwall.data.local.entities.Order
+import com.judahben149.fourthwall.data.repository.OrderRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class GetAllOrdersUseCase @Inject constructor(private val orderRepository: OrderRepository) {
+
+    operator fun invoke(): Flow<List<Order>> {
+        return orderRepository.allOrders
+    }
+}
---
+++ app/src/main/java/com/judahben149/fourthwall/domain/usecase/InsertOrdersUseCase.kt
@@ -0,0 +1,13 @@
+package com.judahben149.fourthwall.domain.usecase
+
+import com.judahben149.fourthwall.data.local.entities.Order
+import com.judahben149.fourthwall.data.repository.OrderRepository
+import javax.inject.Inject
+
+
+class InsertOrdersUseCase @Inject constructor(private val orderRepository: OrderRepository) {
+
+    suspend operator fun invoke(order: Order) {
+        orderRepository.insert(order)
+    }
+}
