5afefe561b50daa449dfa8b1cb3a32bc9633834c
Judah Ben <judahben149@gmail.com>
2024-09-02 11:26:33 +0100

    Created use case module and added use cases to module

---
+++ app/src/main/java/com/judahben149/fourthwall/di/module/UseCaseModule.kt
@@ -0,0 +1,27 @@
+package com.judahben149.fourthwall.di.module
+
+import com.judahben149.fourthwall.data.repository.OrderRepository
+import com.judahben149.fourthwall.domain.usecase.GetAllOrdersUseCase
+import com.judahben149.fourthwall.domain.usecase.InsertOrdersUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun providesInsertOrdersUseCase(orderRepository: OrderRepository): InsertOrdersUseCase {
+        return InsertOrdersUseCase(orderRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun providesGetAllOrdersUseCase(orderRepository: OrderRepository): GetAllOrdersUseCase {
+        return GetAllOrdersUseCase(orderRepository)
+    }
+}
