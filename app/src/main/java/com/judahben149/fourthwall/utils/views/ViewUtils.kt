c183f2e664b96030cd128e838ccdb09693ca8619
Judah Ben <judahben149@gmail.com>
2024-09-03 14:15:42 +0100

    Updated View Utils

--- app/src/main/java/com/judahben149/fourthwall/utils/views/ViewUtils.kt
+++ app/src/main/java/com/judahben149/fourthwall/utils/views/ViewUtils.kt
@@ -3,9 +3,15 @@ package com.judahben149.fourthwall.utils.views
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.content.res.Resources
+import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import com.google.android.material.snackbar.Snackbar
+import com.judahben149.fourthwall.R
 
 fun ImageView.animateBounce() {
     this.animate()
@@ -86,4 +92,47 @@ fun ImageView.setupDeFiNetworkAnimation() {
         playTogether(pulseAnimation, rotateAnimation)
         start()
     }
+}
+
+fun MaterialButton.disable(res: Resources, pgBar: CircularProgressIndicator? = null) {
+    this.setBackgroundColor(res.getColor(R.color.disabledPrimaryBtnBackgroundTint))
+
+    // Disable clicking
+    this.isClickable = false
+    this.isEnabled = false
+
+    pgBar?.let {
+        visibility = View.INVISIBLE
+    }
+}
+
+fun MaterialButton.enable(res: Resources, pgBar: CircularProgressIndicator? = null) {
+    this.setBackgroundColor(res.getColor(R.color.primaryBtnBackgroundTint))
+
+    // Disable clicking
+    this.isClickable = true
+    this.isEnabled = true
+
+    this.textScaleX = 1F
+    this.alpha = 1F
+
+    pgBar?.let {
+        pgBar.visibility = View.INVISIBLE
+    }
+}
+
+fun MaterialButton.isLoading(res: Resources, pgBar: CircularProgressIndicator) {
+    this.setBackgroundColor(res.getColor(R.color.primaryBtnBackgroundTint))
+    this.alpha = 0.6F
+
+    // Disable clicking
+    this.isClickable = false
+    this.isEnabled = true
+
+    this.textScaleX = 0F
+    pgBar.visibility = View.VISIBLE
+}
+
+fun showSnack(message: String, rootView: View, duration: Int = Snackbar.LENGTH_SHORT) {
+    Snackbar.make(rootView, message, duration).show()
 }
