cbf964643608cab0f96e929828a6a3509bcf64be
Judah Ben <judahben149@gmail.com>
2024-09-03 01:41:26 +0100

    Updated constants

--- app/src/main/java/com/judahben149/fourthwall/utils/Constants.kt
+++ app/src/main/java/com/judahben149/fourthwall/utils/Constants.kt
@@ -3,6 +3,9 @@ package com.judahben149.fourthwall.utils
 object Constants {
     // DI Module provider names
     const val FOURTH_WALL_SHARED_PREF = "FOURTH_WALL_SHARED_PREF"
+    const val SHARED_PREFERENCES = "SHARED_PREFERENCES"
+    const val ENCRYPTED_SHARED_PREFERENCES = "ENCRYPTED_SHARED_PREFERENCES"
+    const val FILE_ENCRYPTED_SHARED_PREFERENCES = "FILE_ENCRYPTED_SHARED_PREFERENCES"
 
     // Database
     const val DATABASE_NAME = "FOURTH_WALL_ROOM_DATABASE"
