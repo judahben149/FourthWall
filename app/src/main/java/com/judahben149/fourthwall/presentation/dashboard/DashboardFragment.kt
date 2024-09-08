82e528a4290b3e8c1b00f920c3428611aa378f60
Judah Ben <judahben149@gmail.com>
2024-09-08 01:53:44 +0100

    Begun updating Dashboard screen

--- app/src/main/java/com/judahben149/fourthwall/presentation/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/judahben149/fourthwall/presentation/dashboard/DashboardFragment.kt
@@ -1,20 +1,38 @@
 package com.judahben149.fourthwall.presentation.dashboard
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.judahben149.fourthwall.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.judahben149.fourthwall.databinding.FragmentDashboardBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
+    private val navController by lazy { findNavController() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
